3835814612da9f6cc5716d723949fb52f75fcf3c
Alina-Maximova <alina26.09.2002@yandex.ru>
2024-11-03 15:40:57 +0300

    Add Student class

---
+++ UniversityManagementSystem/DepartmenManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace UniversityManagementSystem
+{
+    public class DepartmenManager
+    {
+        public class Departmen
+        {
+            public string Name { get; set; }
+            public string Code { get; set; }
+
+            public Departmen(string name, string code)
+            {
+                Name = name;
+                Code = code;
+            }
+        }
+
+        private List<Departmen> _institutes = new List<Departmen>();
+
+        public void AddDepartmen(Departmen institute)
+        {
+            if (string.IsNullOrEmpty(institute.Name))
+            {
+                throw new ArgumentException("Departmen name cannot be empty.");
+            }
+
+            _institutes.Add(institute);
+        }
+
+        public List<Departmen> GetDepartmens()
+        {
+            return _institutes;
+        }
+
+        public Departmen FindDepartmenByCode(string code)
+        {
+            return _institutes.Find(i => i.Code == code);
+        }
+
+        public void DeleteDepartmen(string code)
+        {
+            _institutes.RemoveAll(i => i.Code == code);
+        }
+
+        public void UpdateDepartmen(string code, Departmen updatedDepartmen)
+        {
+            var instituteToUpdate = _institutes.Find(i => i.Code == code);
+            if (instituteToUpdate != null)
+            {
+                instituteToUpdate.Name = updatedDepartmen.Name;
+            }
+        }
+    }
+
+
+}
---
+++ UniversityManagementSystem/StudentManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace UniversityManagementSystem
+{
+    public class SrudentManager
+    {
+        public class Srudent
+        {
+            public string Name { get; set; }
+            public string Code { get; set; }
+
+            public Srudent(string name, string code)
+            {
+                Name = name;
+                Code = code;
+            }
+        }
+
+        private List<Srudent> _institutes = new List<Srudent>();
+
+        public void AddSrudent(Srudent institute)
+        {
+            if (string.IsNullOrEmpty(institute.Name))
+            {
+                throw new ArgumentException("Srudent name cannot be empty.");
+            }
+
+            _institutes.Add(institute);
+        }
+
+        public List<Srudent> GetSrudents()
+        {
+            return _institutes;
+        }
+
+        public Srudent FindSrudentByCode(string code)
+        {
+            return _institutes.Find(i => i.Code == code);
+        }
+
+        public void DeleteSrudent(string code)
+        {
+            _institutes.RemoveAll(i => i.Code == code);
+        }
+
+        public void UpdateSrudent(string code, Srudent updatedSrudent)
+        {
+            var instituteToUpdate = _institutes.Find(i => i.Code == code);
+            if (instituteToUpdate != null)
+            {
+                instituteToUpdate.Name = updatedSrudent.Name;
+            }
+        }
+    }
+
+
+}
